3a50772d2589df628e46e5fa2f5334fe81caf594
Giovannalilian <giovanna.lilian@escolar.ifrn.edu.br>
2025-05-05 12:05:43 -0300

    giovanna

---
+++ giovanna/Assets/Scenes/script/Play.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Play : MonoBehaviour
+{
+    private abstract class gameObject
+    {
+        public abstract class transform
+        {
+            public class position
+            {
+            }
+        }
+    }
+
+    private int numero;
+    public int velocidade = 40;
+    void Start()
+    {
+        numero = 0;
+    }
+    void Update()
+    {
+        //Debug.log(numero);
+        //numero = numero + 1;
+        
+ if (Input.GetKey(KeyCode.A))
+ {
+gameObject.transform.position += new Vectot3(velocidade 0, 0);
+}
+
+    if (Input.GetKey(KeyCode.D))
+internal class velocidade
+{
+}
