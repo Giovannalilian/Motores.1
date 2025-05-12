5e210478e02226c3f63e45c6b3853ef76ab640d6
Giovannalilian <giovanna.lilian@escolar.ifrn.edu.br>
2025-05-12 11:53:22 -0300

    marlison prof

---
+++ motores/motores/Assets/Scenes/Play.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Play : MonoBehaviour
+{
+    private int numero = 0;
+    public float velocidade = 40;
+
+    void Start()
+    {
+        numero = 0;
+
+    }
+
+    void Update()
+    {
+        //debug.log(numero0;
+        //numero = numero + 1;
+
+        if (Input.GetKey(KeyCode.A));
+        {
+            gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D));
+
+
+        gameObject.transform.position += new Vector3(+velocidade * Time.deltaTime, 0, 0);
+
+    }
+}
+        
+        
