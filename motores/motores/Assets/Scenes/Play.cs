2fee2c865d5cb0ac8203df92c01aca43a434e2d0
Giovannalilian <giovanna.lilian@escolar.ifrn.edu.br>
2025-05-26 11:48:12 -0300

    câmera

--- motores/motores/Assets/Scenes/Play.cs
+++ motores/motores/Assets/Scenes/Play.cs
@@ -16,16 +16,15 @@ public class Play : MonoBehaviour
         //debug.log(numero0;
         //numero = numero + 1;
 
-        if (Input.GetKey(KeyCode.A));
+        if (Input.GetKey(KeyCode.A))
         {
             gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime, 0, 0);
         }
 
-        if (Input.GetKey(KeyCode.D));
-
-
-        gameObject.transform.position += new Vector3(+velocidade * Time.deltaTime, 0, 0);
-
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.position += new Vector3(+velocidade * Time.deltaTime, 0, 0);
+        }
     }
 }
         
