fcee9938d0cfd4fd7599e8efdb76d9aee9f08dfd
Giovannalilian <giovanna.lilian@escolar.ifrn.edu.br>
2025-06-02 11:41:00 -0300

    junho

--- motores/motores/Assets/Scenes/Play.cs
+++ motores/motores/Assets/Scenes/Play.cs
@@ -2,12 +2,11 @@ using UnityEngine;
 
 public class Play : MonoBehaviour
 {
-    private int numero = 0;
+
     public float velocidade = 40;
 
     void Start()
     {
-        numero = 0;
 
     }
 
@@ -25,7 +24,28 @@ public class Play : MonoBehaviour
         {
             gameObject.transform.position += new Vector3(+velocidade * Time.deltaTime, 0, 0);
         }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(new Vector2(0,forcaDoPulo), ForceMode2D.Impulse);
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D colisao)
+    {
+        //if (colisao.gameObject.tag == "Chao")
+        if(colisao.gameObject.CompareTag("Chao"))
+        {
+            noChao = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D colisao)
+    {
+        if(colisao.gameObject.CompareTag("Chao"))
+        {
+            noChao = false;
+        }
     }
 }
-        
-        
