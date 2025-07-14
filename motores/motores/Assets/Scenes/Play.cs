39139efedf1a50b064e47d4ca082dd1565654d7d
Giovannalilian <giovanna.lilian@escolar.ifrn.edu.br>
2025-07-14 11:46:47 -0300

    etzinho deu certooooooooooooo

--- motores/motores/Assets/Scenes/Play.cs
+++ motores/motores/Assets/Scenes/Play.cs
@@ -1,41 +1,53 @@
+using System;
 using UnityEngine;
 
-public class Play : MonoBehaviour
+public class Player : MonoBehaviour
 {
-
-    public float velocidade = 40;
-
-    private Rigidbody2D rb;
-
-    private bool  noChao = false;
-    public float forcaDoPulo = 3;
     
-    void Start()
-    {
-       rb = GetComponent<Rigidbody2D>();
-    }
+    public float velocidade = 40;
+    public float forcaDoPulo = 4;
+    
+    private bool noChao = false;
+    private bool andando = false;
+    
+    private SpriteRenderer sprite;
+    private Rigidbody2D rb;
+    private Animator animator;
+    
+       void Start()
+       {
+           sprite = GetComponent<SpriteRenderer>();
+           rb = GetComponent<Rigidbody2D>();
+           animator = GetComponent<Animator>();
+       }
 
-    void Update()
+       
+   void Update()
     {
-        //debug.log(numero0;
-        //numero = numero + 1;
-
+        andando = false;
+        
         if (Input.GetKey(KeyCode.A))
         {
-            gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime, 0, 0);
+            gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime,0,0);
+            sprite.flipX = true;
+            andando = true;
         }
-
+        
         if (Input.GetKey(KeyCode.D))
         {
-            gameObject.transform.position += new Vector3(+velocidade * Time.deltaTime, 0, 0);
+            gameObject.transform.position += new Vector3(velocidade * Time.deltaTime,0,0);
+            sprite.flipX = false;
+            andando = true;
         }
 
-        if (Input.GetKey(KeyCode.Space) && noChao == true)
+        if (Input.GetKeyDown(KeyCode.Space) && noChao == true)
         {
-            float forcaDoPulo = 3;
-            rb.AddForce(new Vector2(0,y: forcaDoPulo), ForceMode2D.Impulse);
+            rb.AddForce(new Vector2(0,forcaDoPulo), ForceMode2D.Impulse);
         }
 
+        animator.SetBool("Andando",andando);
+        animator.SetBool("Pulo",!noChao);
+        
     }
 
     void OnCollisionEnter2D(Collision2D colisao)
@@ -43,15 +55,15 @@ public class Play : MonoBehaviour
         //if (colisao.gameObject.tag == "Chao")
         if(colisao.gameObject.CompareTag("Chao"))
         {
-           noChao = true;
+            noChao = true;
         }
     }
 
     void OnCollisionExit2D(Collision2D colisao)
     {
         if(colisao.gameObject.CompareTag("Chao"))
-        { 
+        {
             noChao = false;
         }
-    } 
+    }
 }
